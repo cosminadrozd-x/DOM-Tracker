4edcebd08982ad1c1e90de30dfc036d40bfd4888
cosminadrozd-x <cosminadrozd@gmail.com>
2026-03-11 21:11:13 +0100

    Étape 3 - micro-tracker complet

--- tracker.js
+++ tracker.js
@@ -1,18 +1,64 @@
-let total = document.getElementById('val_total');
-
-let btn1 = document.getElementById('btn_add1');
-let btn5 = document.getElementById('btn_add5');
-
-
-btn1.addEventListener('click', () => {
-
-    total.innerText = parseInt(total.innerText) + 1;
-
-});
+// variables d'état
+let sun = 0
+let water = 0
+let breakTime = 0
 
 
-btn5.addEventListener('click', () => {
+// récupération des éléments HTML
+let sunValue = document.getElementById("sun-value")
+let waterValue = document.getElementById("water-value")
+let breakValue = document.getElementById("break-value")
+let totalValue = document.getElementById("total-value")
 
-    total.innerText = parseInt(total.innerText) + 5;
+let btnSun = document.getElementById("btn-sun")
+let btnWater = document.getElementById("btn-water")
+let btnBreak = document.getElementById("btn-break")
+let btnReset = document.getElementById("btn-reset")
 
-});
+
+// bouton Soleil
+btnSun.addEventListener("click", () => {
+
+    sun = sun + 1
+    sunValue.innerText = sun
+
+    totalValue.innerText = sun + water + breakTime
+
+})
+
+
+// bouton Eau
+btnWater.addEventListener("click", () => {
+
+    water = water + 1
+    waterValue.innerText = water
+
+    totalValue.innerText = sun + water + breakTime
+
+})
+
+
+// bouton Pause
+btnBreak.addEventListener("click", () => {
+
+    breakTime = breakTime + 1
+    breakValue.innerText = breakTime
+
+    totalValue.innerText = sun + water + breakTime
+
+})
+
+
+// bouton reset
+btnReset.addEventListener("click", () => {
+
+    sun = 0
+    water = 0
+    breakTime = 0
+
+    sunValue.innerText = 0
+    waterValue.innerText = 0
+    breakValue.innerText = 0
+    totalValue.innerText = 0
+
+})
